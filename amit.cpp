d847d74982820fe0a782b0c65899a184494d7e1c
galtoubul <galtoubul@gmail.com>
2020-03-15 12:15:59 +0200

    Amit Comment

---
+++ amit.cpp
@@ -0,0 +1,5 @@
+//
+// Created by galto on 15/03/2020.
+//
+
+#include "amit.h"
---
+++ amit.h
@@ -0,0 +1,14 @@
+//
+// Created by galto on 15/03/2020.
+//
+
+#ifndef SUDOKU_AMIT_H
+#define SUDOKU_AMIT_H
+
+
+class amit {
+
+};
+
+
+#endif //SUDOKU_AMIT_H
