bd031bfa0c97bbda6923b2dbd9a898465876d069
omrib40 <40802287+omrib40@users.noreply.github.com>
2020-03-24 12:10:50 +0200

    deleted comments

--- Game.c
+++ Game.c
@@ -327,7 +327,6 @@ void freeLinkedList(Node* node,int rows){
     Node* temp;
     clearRedoNodes(node->next,rows);
     while(node != NULL){
-        /*TODO there's a problem here if we insert edit as first command it collapses at the second iteration*/
         temp = node->prev;
         freeNode(node,rows);
         node = temp;
